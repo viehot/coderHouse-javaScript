0a6f990ed80e2fc532c9384991c188fe46f12893
Cristian <piri.viejo@gmail.com>
2022-04-12 21:51:46 -0300

    Ejercicio finalizado

--- ejercicio1/js/myScript.js
+++ ejercicio1/js/myScript.js
@@ -1,3 +1,13 @@
+let seleccion
 do {
     let seleccion = prompt("Bienvenido\nSeleccione una opcion.\n1.Agregar Persona.\n2.Salir")
-} while (condition);
+    if (seleccion == 1) {
+        let nombre = prompt("Escriba su nombre")
+        let apellido = prompt("Escriba su apellido")
+        console.log("Hola " + nombre + " " + apellido)
+    } else if(seleccion == 2) {
+        console.log("Gracias por su visita")
+    } else {
+        console.log("No selecciono una opcion correcta")
+    }
+} while (seleccion == 2);
